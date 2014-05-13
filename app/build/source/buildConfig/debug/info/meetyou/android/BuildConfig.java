956fdc2c481d9d75d9e6b3143728bfc4ea866d0c
Vladislav Meshochkin <cloudkicker@autistici.org>
2014-05-13 04:10:24 +0400

    Добавил каталог сборки Подписанный APK называется app-debug-unaligned.apk

---
+++ app/build/source/buildConfig/debug/info/meetyou/android/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package info.meetyou.android;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "info.meetyou.android.debug";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1000;
+  public static final String VERSION_NAME = "1.0.0 Debug";
+}
